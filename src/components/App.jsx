bb53947eb0fb2b0e66a2e2bca1679d31e8a1ec87
AVasilaki <poseydon463@gmail.com>
2023-10-08 11:04:42 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,29 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
-
+  changeGood = newState =>
+    this.setState({
+      good: newState,
+    });
+  changeNeutral = newState =>
+    this.setState({
+      neutral: newState,
+    });
+  changeBad = newState =>
+    this.setState({
+      bad: newState,
+    });
   render() {
     const state = this.state;
 
     return (
       <div>
-        <Feedback state={state}></Feedback>
+        <Feedback
+          state={state}
+          onChangeGood={this.changeGood}
+          onChangeNeutral={this.changeNeutral}
+          onChangeBad={this.changeBad}
+        ></Feedback>
       </div>
     );
   }
--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,20 +1,25 @@
-export const Feedback = ({ state }) => {
+export const Feedback = ({
+  state: { good, neutral, bad },
+  onChangeGood,
+  onChangeNeutral,
+  onChangeBad,
+}) => {
   return (
     <div>
       <h2>Please leave feedback</h2>
-      <button type="button" onClick={() => {}}>
-        good
+      <button type="button" onClick={() => onChangeGood((good += 1))}>
+        Good
       </button>
-      <button type="button" onClick={() => {}}>
+      <button type="button" onClick={() => onChangeNeutral((neutral += 1))}>
         Neutral
       </button>
-      <button type="button" onClick={() => {}}>
+      <button type="button" onClick={() => onChangeBad((bad += 1))}>
         Bad
       </button>
       <h2>Statistic</h2>
-      <p>Good:{state.good}</p>
-      <p>Neutral:{state.neutral}</p>
-      <p>Bad:{state.bad} </p>
+      <p>Good:{good}</p>
+      <p>Neutral:{neutral}</p>
+      <p>Bad:{bad} </p>
     </div>
   );
 };
