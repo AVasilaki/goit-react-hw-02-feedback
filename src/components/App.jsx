ffceefc0095777f3c426de3b96a030c0bfb9b5dc
AVasilaki <poseydon463@gmail.com>
2023-10-08 18:04:10 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,40 @@
 import { Component } from 'react';
 import { Feedback } from './feedback/Feedback';
-// import { LoginForm } from './loginforn';
+
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
+
   changeGood = newState =>
     this.setState({
       good: newState,
     });
+
   changeNeutral = newState =>
     this.setState({
       neutral: newState,
     });
+
   changeBad = newState =>
     this.setState({
       bad: newState,
     });
+
+  countTotalFeedback = total =>
+    (total = this.state.good + this.state.neutral + this.state.bad);
+
+  countPositiveFeedbackPercentage = percentege => {
+    const state = this.state;
+    const total = state.good + state.neutral + state.bad;
+
+    if (total !== 0) {
+      return Math.ceil((percentege = (state.good / total) * 100));
+    }
+  };
+
   render() {
     const state = this.state;
 
@@ -29,6 +45,8 @@ export class App extends Component {
           onChangeGood={this.changeGood}
           onChangeNeutral={this.changeNeutral}
           onChangeBad={this.changeBad}
+          countTotalFeedback={this.countTotalFeedback}
+          countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
         ></Feedback>
       </div>
     );
--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -3,6 +3,8 @@ export const Feedback = ({
   onChangeGood,
   onChangeNeutral,
   onChangeBad,
+  countTotalFeedback,
+  countPositiveFeedbackPercentage,
 }) => {
   return (
     <div>
@@ -20,6 +22,8 @@ export const Feedback = ({
       <p>Good:{good}</p>
       <p>Neutral:{neutral}</p>
       <p>Bad:{bad} </p>
+      <p>Total:{countTotalFeedback()}</p>
+      <p>Positive feedback:{countPositiveFeedbackPercentage()}%</p>
     </div>
   );
 };
